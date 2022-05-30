aec7bf916a0bf7c0b9a18573a9eadbbeb3bf7f7c
Starik2005 <95094606+Starik2005@users.noreply.github.com>
2022-05-30 07:15:36 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,7 @@ int main()
 
     zip_file_t * fl = zip_fopen(zipper, "final_5/META-INF/entry_point.xml", zf);
     char* buf = new char[10000]{0};
-    char* tagName = new char[10000]{0};
-    char* data = new char[10000]{0};
     int rd = {0};
-    int t = {0};
-    int d = {0};
     int b = {0};
     bool otag = 0;
     bool oname = 0;
@@ -28,38 +24,29 @@ int main()
     {
        b = 0;
        for(; rd>0; rd--){
-           //std::cout << buf[b++];
              if (otag)
                  if (buf[b]=='>'||buf[b]==' '||buf[b]=='/') {
                      otag = 0;
                      t = 0;
                      if(buf[b]=='>') {oname=1; b++; std::cout << " DATA: "; continue; }
-                     } else std::cout << buf[b];//tagName[t++] = buf[b];
+                     } else std::cout << buf[b];
              if(buf[b]=='<') {
                      otag = 1;
                      std::cout << '\n' << "TAG: ";
                      if (oname) {
                          oname = 0;
-                         //data[d] = 0;
-                         //std::cout << data << ": DATA\n";
                          d = 0;
                      }
                  }
-            if(oname)std::cout << buf[b];   //data[d++] = buf[b];
-
-            //if(oname)data[d++] = buf[b];
+            if(oname)std::cout << buf[b];
             b++;
         }
     }
-    //if(t!=tagName)std::cout << tagName;
 
     zip_close(zipper);
 
     std::cout << '\n';
-
     std::cout << num << '\n';
-    // for(auto i: files)
-    //    std::cout << i << '\n';
 
  return 0;
 }
